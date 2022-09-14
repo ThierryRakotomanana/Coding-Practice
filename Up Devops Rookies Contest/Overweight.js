339cab2d0b05df4c7455310854b999f27f55cc47
Thierry Rakotomanana <ThierryRktm@gmail.com>
2022-09-14 15:42:10 +0300

    fix: parseInt string value

--- Up Devops Rookies Contest/Overweight.js
+++ Up Devops Rookies Contest/Overweight.js
@@ -1,12 +1,13 @@
 function processData(input) {
-    input = input.split('\n')
-    let length = input[0], value = [], test = 0, number = [], choice = 0;
+    input = input.trim().split('\n')
+    let length = parseInt(input[0]), value = [], test = 0, number = [], choice;
     input.shift()
     for (let i = 0; i <  length ; i++) {
-        number = input[2*i].split(' ')
-        value = input[2*i+1].split(' ').sort((a, b) => a - b)
-        console.log(value)
-        for (let k = 0; k < number[1] ; k++) {
+        number = input[2*i].trim().split(" ")
+        value = input[2*i+1].trim().split(' ').sort((a, b) => parseInt(a) - parseInt(b))
+        choice = 0
+        test = 0;
+        for (let k = 0; k < parseInt(value.length) ; k++) {
             if(test + parseInt(value[k]) < parseInt(number[0])){
                 choice++
                 test+=parseInt(value[k])
@@ -14,11 +15,9 @@ function processData(input) {
                 break
             }
         }
-        console.log(choice)
-        choice = 0
-        test = 0;
+        console.log(choice);
     }
 }
 
 let input = "3\n10 5\n2 5 3 4 6\n100 7\n10 11 10 22 15 35 25\n55 8\n55 5 30 30 70 15 21 33"
-console.log(processData(input))
+let final = processData(input)
