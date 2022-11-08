73ba68ab3b3ad1a3cc03b41a4fd5ec71d3e21495
Thierry Rakotomanana <ThierryRktm@gmail.com>
2022-11-08 22:02:55 +0300

    fix: state error don't load

--- daily Javascript Project/client_side_form_validation/app.js
+++ daily Javascript Project/client_side_form_validation/app.js
@@ -1,9 +1,35 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password');
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
-})
+
+    checkInputs();
+});
+
+function checkInputs(){
+    //get the values from the inputs
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === ""){
+        // show error
+        setErrorFor(username, 'Username cannot be blank')
+    } else {
+        setSuccessFor(username)
+    }
+}
+
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    small.innerText = message;
+
+    formControl.className = "form-control error";
+}
